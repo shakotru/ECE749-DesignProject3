d1939621f3136cbe2f731f04e436e103625cedf4
Sharda Kotru <sharda.kotru@gmail.com>
2026-03-16 14:59:23 -0400

    adding defines file

---
+++ 01_RTL/defines.v
@@ -0,0 +1,38 @@
+`define  OP_LOAD 		 4'b0000
+`define  OP_SHIFT_RIGHT  	 4'b0100
+`define  OP_SHIFT_LEFT 		 4'b0101
+`define  OP_SHIFT_UP 		 4'b0110
+`define  OP_SHIFT_DOWN 		 4'b0111
+`define  OP_SCALE_DOWN 		 4'b1000
+`define  OP_SCALE_UP 		 4'b1001
+`define  OP_MEDIAN_FILTER 	 4'b1100
+`define  OP_YCBCR 		 4'b1101
+`define  OP_CENSUS 		 4'b1110
+
+`define SCALE_16  2'd2
+`define SCALE_8  2'd1
+`define SCALE_4  2'd0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
